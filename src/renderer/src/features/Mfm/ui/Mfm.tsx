9ea6188d1d81a3679687c7d9c4ec7637130b973c
tahirdibirov <tahirdibirovsecond@gmail.com>
2024-04-20 16:04:07 +0400

    MFM System UI implemented

--- src/renderer/src/features/Mfm/ui/Mfm.tsx
+++ src/renderer/src/features/Mfm/ui/Mfm.tsx
@@ -1,9 +1,26 @@
+import { Button } from 'antd'
 import style from './Mfm.module.scss'
+import { useTerminalSkin } from '@renderer/entities/Terminal/lib'
 
 const Mfm = ():JSX.Element => {
+
+    const staticValues = [1,2,3,4,5,6,7,8,9, 'R','G','B']
+
     return(
-        <div className={style.Mfm}>
-            <h1>MFM</h1>
+        <div style={useTerminalSkin()} className={style.Mfm}>
+            <select style={useTerminalSkin()} className={style.Selector} name="" id="">
+                {staticValues.map(value=><option className={style.Option} key={value} value={value}>{value}</option>)}
+            </select>
+            <select style={useTerminalSkin()} className={style.Selector} name="" id="">
+                 {staticValues.map(value=><option className={style.Option} key={value} value={value}>{value}</option>)}
+            </select>
+            <select style={useTerminalSkin()} className={style.Selector} name="" id="">
+                {staticValues.map(value=><option className={style.Option} key={value} value={value}>{value}</option>)}
+            </select>
+            <select style={useTerminalSkin()} className={style.Selector} name="" id="">
+                 {staticValues.map(value=><option className={style.Option} key={value} value={value}>{value}</option>)}
+            </select>
+            <Button style={useTerminalSkin()}>SEND</Button>
         </div>
     )
 }
