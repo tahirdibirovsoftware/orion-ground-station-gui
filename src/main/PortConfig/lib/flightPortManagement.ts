dbae1bfb7ff0f8628a74d2f3d074dbfac8309ca0
tahirdibirov <tahirdibirovsecond@gmail.com>
2024-05-16 01:01:01 +0400

    UI Data Flow has been implemented

--- src/main/PortConfig/lib/flightPortManagement.ts
+++ src/main/PortConfig/lib/flightPortManagement.ts
@@ -1,7 +1,7 @@
 import { ReadlineParser, SerialPort } from "serialport";
 import { ITelemetry, SatStatus } from '../../../global/types/types'
 
-const mockTelemetry = '154*0*11111*0:0:2000:0:0:0*1008.78*0.00*10.13*0.00*10.13*9.93*22.41*8.40*0.000000*0.000000*0.00*59.77*101.63*181.80*0N0N*0.00*488776'
+// const mockTelemetry = '154*0*11111*0:0:2000:0:0:0*1008.78*0.00*10.13*0.00*10.13*9.93*22.41*8.40*0.000000*0.000000*0.00*59.77*101.63*181.80*0N0N*0.00*488776'
 
 export const dataParser = (data: string, delimetr: string): ITelemetry | string => {
 
--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,13 +1,15 @@
 import { contextBridge, ipcRenderer } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 import { SerialPort } from 'serialport'
+import { ITelemetry } from '../global/types/types';
 
 // Custom APIs for renderer
 const api = {
   getPortList: (): Promise<ReturnType<typeof SerialPort.list>>=>ipcRenderer.invoke('port-list'),
   onPortListUpdated: (callback: (ports: ReturnType<typeof SerialPort.list>) => void):void => {
     ipcRenderer.on('port-list-updated', (event, ports) => callback(ports));
-  }
+  },
+  connectToFlight: (path:string, baudRate:number):Promise<ITelemetry> => ipcRenderer.invoke('connect-to-flight', {path, baudRate})
 }
 
 // Use `contextBridge` APIs to expose Electron APIs to
