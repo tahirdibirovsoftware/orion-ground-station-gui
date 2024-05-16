5b92e04f7676c6c032b0ca0eb78826198db1a0ac
Tahir Dibirov II <tahirdibirovsecond@gmail.com>
2024-05-16 17:03:27 +0400

    Data fetching on Render

--- src/renderer/src/features/PageSwitcher/ui/PageSwitcher.tsx
+++ src/renderer/src/features/PageSwitcher/ui/PageSwitcher.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import style from './PageSwitcher.module.scss'
 import { FC, useContext } from 'react'
 import { IPageSwitcher } from '../model/types'
