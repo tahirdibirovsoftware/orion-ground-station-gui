e2c5483025b784f53344e4b269b7c2377a05d157
Tahir Dibirov II <tahirdibirovsecond@gmail.com>
2024-05-22 16:44:47 +0400

    Battery level corrected

--- src/renderer/src/widgets/StatusBar/lib/getBatteryLevel.ts
+++ src/renderer/src/widgets/StatusBar/lib/getBatteryLevel.ts
@@ -1,4 +1,16 @@
-export const getBatteryLevel = (currentVoltage:number, maxVoltage:number):number=>{
-    return currentVoltage*100/maxVoltage
+
+
+export const getBatteryLevel = (currentVoltage: number):number => {
+    const maxVoltage = 8.4;
+    const minVoltage = 7.0;
+
+    if (currentVoltage > maxVoltage) {
+        return 100.0;
+    } else if (currentVoltage < minVoltage) {
+        return 0.0;
+    } else {
+        const percentage = ((currentVoltage - minVoltage) / (maxVoltage - minVoltage)) * 100;
+        return percentage;
+    }
 }
 
