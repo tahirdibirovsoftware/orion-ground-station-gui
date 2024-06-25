fde7ec0c7ee739477ebafb820cab118ef38c01ab
tahirdibirov <tahirdibirovsecond@gmail.com>
2024-06-26 00:56:40 +0400

    ----

--- src/renderer/src/entities/SatStatus/ui/SatStatus.tsx
+++ src/renderer/src/entities/SatStatus/ui/SatStatus.tsx
@@ -3,32 +3,33 @@ import style from './SatStatus.module.scss';
 import { ThemeContext } from '../../../app/providers/ThemeProvider/ThemeProvider';
 import { themeSetter } from '../../../shared/config/theme/themeSetter';
 import { ISatStatus } from '../model/types';
+import { Trans, useTranslation } from 'react-i18next';
 
 
-const SatStatus:FC<ISatStatus> = ({flightData}):JSX.Element => {
-
-    const satStatus = flightData[flightData.length-1].satelliteStatus
-    const isAvailable = Boolean(flightData[flightData.length-1].packetNumber)
+const SatStatus: FC<ISatStatus> = ({ flightData }): JSX.Element => {
+    useTranslation()
+    const satStatus = flightData[flightData.length - 1].satelliteStatus
+    const isAvailable = Boolean(flightData[flightData.length - 1].packetNumber)
     const { theme } = useContext(ThemeContext)
 
-    const localStyles:React.CSSProperties = {
-        ...themeSetter(theme)        
+    const localStyles: React.CSSProperties = {
+        ...themeSetter(theme)
     }
 
     const satStatuses = [
-        'Ready to Flight',
-        'Ascent',
-        'Model Satellite Descent',
-        'Release',
-        'Science Payload Descent',
-        'Recovery'
+        "READY_TO_FLIGHT",
+        "ASCENT",
+        "MODEL_SATELLITE_DESCENT",
+        "RELEASE",
+        "SCIENCE_PAYLOAD_DESCENT",
+        "RECOVERY"
     ]
 
-    return(
+    return (
         <div style={localStyles} className={style.SatStatus}>
-            <span>{isAvailable && satStatuses[satStatus]?.toUpperCase()}</span>
+            <span>{isAvailable && <Trans>{satStatuses[satStatus]?.toUpperCase()}</Trans>}</span>
         </div>
     )
 }
 
-export {SatStatus}
+export { SatStatus }
