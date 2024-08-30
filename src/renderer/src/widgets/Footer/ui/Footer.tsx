7667c985926b91b3a68b71b16b02edf2cacdfc5e
Tahir Dibirov II <tahirdibirovsecond@gmail.com>
2024-08-30 15:35:53 +0400

    NAA logo added

--- src/renderer/src/widgets/Footer/ui/Footer.tsx
+++ src/renderer/src/widgets/Footer/ui/Footer.tsx
@@ -2,6 +2,7 @@ import React, { useContext, useMemo } from 'react';
 import style from './Footer.module.scss';
 import teknofestLogoBlack from '../assets/teknofest-black.png';
 import teknofestLogoWhite from '../assets/teknofest-white.png';
+import naaLogoWhite from '../assets/naa-logo-white.png'
 import { themeSetter } from '../../../shared/config/theme/model/themeSetter';
 import { ThemeContext } from '../../../app/providers/ThemeProvider/ThemeProvider';
 import { Logo } from '../../../entities/Logo';
@@ -29,10 +30,15 @@ const Footer: React.FC = React.memo(() => {
         src={teknofestLogo} 
         alt="Teknofest Logo"
       />
-      <span className={style.naaLogo}>
+      {/* <span className={style.naaLogo}>
         {'National Aviation Academy'.toUpperCase()}
-      </span>
-      <Logo size={3} title='Orion' />
+      </span> */}
+      <img 
+        className={style.teknofestLogo} 
+        src={naaLogoWhite} 
+        alt="Teknofest Logo"
+      />
+      <Logo size={2.6} title='Orion' />
       <span className={style.copy}>&copy; {currentYear}, Teknofest</span>
     </footer>
   );
